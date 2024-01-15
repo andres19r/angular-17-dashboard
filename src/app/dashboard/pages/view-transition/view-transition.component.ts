7cb7799a02b454044e764e507a595b8d1efa2c6e
Andres Rivero <andres19rivero@gmail.com>
2024-01-15 13:47:24 -0400

    feat!: generate files & directories structure

---
+++ src/app/dashboard/pages/view-transition/view-transition.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-transition',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-transition.component.html',
+  styleUrl: './view-transition.component.scss'
+})
+export class ViewTransitionComponent {
+
+}
