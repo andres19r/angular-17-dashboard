05e394c53d7630711fdf4096fb115b25e89b6b95
Andres Rivero <andres19rivero@gmail.com>
2024-01-15 17:07:00 -0400

    feat: implement sidemenu & dashboard initial templates

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
   imports: [],
   templateUrl: './sidemenu.component.html',
-  styleUrl: './sidemenu.component.scss'
+  styleUrl: './sidemenu.component.scss',
 })
 export class SidemenuComponent {
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
 
+  constructor() {
+    // const dashboardRoute = routes
+    //   .map((route) => route.children ?? [])
+    //   .flat()
+    //   .filter((route) => route && route.path)
+    //   .filter((route) => !route.path?.includes(':'));
+    // console.log(dashboardRoute);
+  }
 }
