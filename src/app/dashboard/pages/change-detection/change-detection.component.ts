f0b4ff4ff8683ee1203dd820f591662f28b6b163
Andres Rivero <andres19rivero@gmail.com>
2024-01-16 12:25:53 -0400

    feat(change-detection): implement template to compare detection strategies

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TitleComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './change-detection.component.html',
   styleUrl: './change-detection.component.scss',
 })
-export default class ChangeDetectionComponent {}
+export default class ChangeDetectionComponent {
+  public currentFramework = computed(
+    () => `Change detection - ${this.frameworkAsSignal().name}`,
+  );
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // this.frameworkAsProperty.name = 'Vue';
+      this.frameworkAsSignal.update((value) => ({
+        ...value,
+        name: 'React',
+      }));
+      console.log('done');
+    }, 2000);
+  }
+}
