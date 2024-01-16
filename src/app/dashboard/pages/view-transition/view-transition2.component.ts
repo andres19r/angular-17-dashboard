87e9da9ed252ed2c871532a15083634c8b01ad22
Andres Rivero <andres19rivero@gmail.com>
2024-01-16 13:36:46 -0400

    feat(view-transition): implement templates to both components & add new route

---
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+
+@Component({
+  selector: 'app-view-transition',
+  standalone: true,
+  imports: [TitleComponent],
+  template: `
+    <app-title title="View Transition 2" />
+
+    <section class="flex justify-start">
+      <img
+        srcset="https://picsum.photos/id/237/200/300"
+        alt="Picums"
+        widht="200"
+        height="300"
+      />
+
+      <div class="bg-blue-500 w-56 h-56"></div>
+    </section>
+  `,
+})
+export default class ViewTransitionComponent {}
