d61f261dd7d07ef96ae6380d4b623306e6d17b60
Andres Rivero <andres19rivero@gmail.com>
2024-01-16 11:20:39 -0400

    feat(title): implement template to show required input

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, booleanAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [],
   templateUrl: './title.component.html',
-  styleUrl: './title.component.scss'
+  styleUrl: './title.component.scss',
 })
 export class TitleComponent {
-
+  @Input({ required: true }) title!: string;
+  @Input({ transform: booleanAttribute }) withShadow: boolean = false;
 }
