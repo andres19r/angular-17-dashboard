8da254711f203a388cfa8e7b7deacec4b16dc705
Andres Rivero <andres19rivero@gmail.com>
2024-01-16 17:04:19 -0400

    feat(user): create signal to show user information

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TitleComponent } from '@shared/title/title.component';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -21,6 +21,12 @@ export default class UserComponent {
     this.route.params.pipe(
       switchMap(({ id }) => this.usersService.getUserById(id)),
     ),
+  );
+
+  public titleLabel = computed(() =>
+    this.user()
+      ? `${this.user()?.first_name} ${this.user()?.last_name}`
+      : 'User information',
   );
 
   // constructor() {
