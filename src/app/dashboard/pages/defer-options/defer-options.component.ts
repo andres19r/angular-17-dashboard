ac2db2d6b54ff713b287aa5ed6cc0a2954b44935
Andres Rivero <andres19rivero@gmail.com>
2024-01-16 13:15:29 -0400

    feat(defer-options): show heavy-loader-fast on interaction

--- src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeavyLoadersFastComponent } from '@shared/heavy-loaders/heavy-loaders-fast/heavy-loaders-fast.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-defer-options',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, HeavyLoadersFastComponent],
   templateUrl: './defer-options.component.html',
   styleUrl: './defer-options.component.scss',
 })
